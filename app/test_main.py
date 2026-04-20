3cee2e0f8ef57b4cfc9d77d5039c1c5c2565e8a6
Ilona <ilonanikolaychuk052@gmail.com>
2026-04-20 21:30:03 +0300

    write tests for cryptocurrency_action function

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,16 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+def test_get_exchange_rate_prediction() -> None:
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mock_prediction):
+        mock_prediction.return_value = 105
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+        mock_prediction.return_value = 95
+        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+        mock_prediction.return_value = 1
+        assert cryptocurrency_action(1) == "Do nothing"
